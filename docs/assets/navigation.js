a7f266724b4bd8ba11505b97a4fa822edca805e1
Daniel Bartholomae <daniel@bartholomae.name>
2024-04-22 19:27:07 +0200

    chore: upgrade typedoc

---
+++ docs/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAE4uuVipJrShRslJKSU1LLM0pUdJRKkgsyQAK5OanlOakFutllOTmKCNkszPzUpSsTAwtTYwNTGp14PqdcxKLi8MSczJTEkvyi3wzU1JyUssTi1IRBiaDVKQW6+NSCbYJboOhkQWS6eGZJRlO+SmVCNNKKguAZsHEUfUaGViaG5oa1cYCAJPAD//fAAAA"
